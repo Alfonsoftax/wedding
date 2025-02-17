08c4886eb2862270ad8af7ca165abab6657881e4
ahidalgo <ahidalgo@izertis.com>
2025-02-17 13:48:09 +0100

    correccion nombres

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'wedding-ppymj'`, () => {
+  it(`should have as title 'wedding'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('wedding-ppymj');
+    expect(app.title).toEqual('wedding');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('wedding-ppymj app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('wedding app is running!');
   });
 });
